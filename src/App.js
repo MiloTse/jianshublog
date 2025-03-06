559498a58a596b7666d5e91a68756db4cf92345b
HuacongXie <solarsys@outlook.com>
2025-03-06 16:16:10 -0500

    1.imported BrowserRouter, Route, Routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component} from 'react';
 import Header from "./common/header";
 import store from "./store";
 import { Provider } from 'react-redux';
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 //store passing data to Header
 function App() {
   return (
       <Provider store={store}>
+        <div>
+            <Header />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' exact render={()=><div>home</div>}></Route>
+                    <Route path='/detail' exact render={()=><div>detail</div>}></Route>
+                </Routes>
 
-        <Header />
+            </BrowserRouter>
+
+        </div>
+
       </Provider>
 
   );
