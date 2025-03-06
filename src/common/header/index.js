08a4f3869ad26e139d57a80eeac9739445dd7606
HuacongXie <solarsys@outlook.com>
2025-03-05 22:03:41 -0500

    1.changed back Header to normal component to avoid unmaintainable Header when passing more data or add more feature.

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { Component } from 'react';
 import '../../statics/iconfont/iconfont.css'; // 引入 iconfont.css
 import { CSSTransition } from "react-transition-group";
 import {connect} from "react-redux";
@@ -19,79 +19,82 @@ import {
     Button
 } from "./style";
 
-
-const getListArea = (show) => {
-    if(show){
-        return (
-
-            <SearchInfo>
-                <SearchInfoTitle>
-                    热门搜索
-                    <SearchInfoSwitch>
-                        换一批
-                    </SearchInfoSwitch>
-                </SearchInfoTitle>
-                <SearchInfoList>
-                    <SearchInfoItem>教育</SearchInfoItem>
-                    <SearchInfoItem>教育</SearchInfoItem>
-                    <SearchInfoItem>教育</SearchInfoItem>
-                    <SearchInfoItem>教育</SearchInfoItem>
-                    <SearchInfoItem>教育</SearchInfoItem>
-                </SearchInfoList>
-            </SearchInfo>
-        )
-    }else{
-        return null;
+class Header extends Component{
+    constructor(props) {
+        super(props);
+        this.searchRef = React.createRef();
     }
 
-}
+    getListArea (show) {
+        if(show){
+            return (
 
-const Header = (props) => {
-    const nodeRef = useRef(null);
+                <SearchInfo>
+                    <SearchInfoTitle>
+                        热门搜索
+                        <SearchInfoSwitch>
+                            换一批
+                        </SearchInfoSwitch>
+                    </SearchInfoTitle>
+                    <SearchInfoList>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                    </SearchInfoList>
+                </SearchInfo>
+            )
+        }else{
+            return null;
+        }
 
-    return (
-        <HeaderWrapper>
-            <Logo />
-            <Nav>
-                <NavItem className='left active'>首页</NavItem>
-                <NavItem className='left'>下载App</NavItem>
-                <NavItem className='right'>登录</NavItem>
-                <NavItem className='right'>
-                    <span className="iconfont">&#xe636;</span>
-                </NavItem>
-                <SearchWrapper>
-                    <CSSTransition
-                        in={props.focused}
-                        timeout={200}
-                        classNames="slide"
-                        nodeRef={nodeRef}
-                    >
-                        <NavSearch
-                            ref={nodeRef}
-                            className={props.focused ? 'focused': ''}
-                            onFocus={props.handleInputFocus}
-                            onBlur={props.handleInputBlur}
+    }
+
+    render() {
+        return (
+            <HeaderWrapper>
+                <Logo />
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载App</NavItem>
+                    <NavItem className='right'>登录</NavItem>
+                    <NavItem className='right'>
+                        <span className="iconfont">&#xe636;</span>
+                    </NavItem>
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={this.props.focused}
+                            timeout={200}
+                            classNames="slide"
+                            nodeRef={this.searchRef}
                         >
-                        </NavSearch>
-                    </CSSTransition>
+                            <NavSearch
+                                ref={el => this.searchRef = el}
+                                className={this.props.focused ? 'focused': ''}
+                                onFocus={this.props.handleInputFocus}
+                                onBlur={this.props.handleInputBlur}
+                            >
+                            </NavSearch>
+                        </CSSTransition>
 
-                    <span className={props.focused ? 'focused iconfont': 'iconfont'}>
+                        <span className={this.props.focused ? 'focused iconfont': 'iconfont'}>
                         &#xe633;
                     </span>
-                     {getListArea(props.focused)}
-                </SearchWrapper>
-            </Nav>
-            <Addition>
-                <Button className='writting'>
-                    写文章
-                     <span className="iconfont">&#xe629;</span>
-                </Button>
-                <Button className='reg'>注册</Button>
-            </Addition>
-        </HeaderWrapper>
-    );
+                        {this.getListArea(this.props.focused)}
+                    </SearchWrapper>
+                </Nav>
+                <Addition>
+                    <Button className='writting'>
+                        写文章
+                        <span className="iconfont">&#xe629;</span>
+                    </Button>
+                    <Button className='reg'>注册</Button>
+                </Addition>
+            </HeaderWrapper>
+        );
+    }
 }
-
 
 const mapStateToProps = (state) => {
     return {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { combineReducers } from 'redux-immutable';
+import store from './store';
 import './style.js';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-
