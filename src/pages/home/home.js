b528708b0515e5df9e406a455d1dca133d5d851a
HuacongXie <solarsys@outlook.com>
2025-03-06 18:32:33 -0500

    1.added img path

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,24 @@
 import React ,{ Component} from "react";
-import { HomeWrapper } from './homeStyle';
+import {
+    HomeWrapper,
+    HomeLeft,
+    HomeRight
+
+} from './homeStyle';
 
 
 class Home extends Component {
     render() {
         return (
-            <HomeWrapper>Home</HomeWrapper>
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className='banner-img' src="https://upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip/imageView2/1/w/1250/h/540"/>
+                </HomeLeft>
+                <HomeRight>
+                    right
+                </HomeRight>
+
+            </HomeWrapper>
         )
     }
 }
