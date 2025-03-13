5b3e31db36dc6067e85772a824f2507377f4df73
HuacongXie <solarsys@outlook.com>
2025-03-13 19:18:05 -0400

    1.used redux to manage detail page's data 2.used dangerouslySetInnerHTML to avoid being transferred

--- src/pages/detail/detail.js
+++ src/pages/detail/detail.js
@@ -1,30 +1,22 @@
 import React ,{ Component} from "react";
 import { DetailWrapper, DetailHeader, Content} from './style';
-import articleImage from '../../statics/article_pic/01.jpeg';
-
+import { connect} from "react-redux";
 class Detail extends Component {
 
   
     render() {
         return (
             <DetailWrapper>
-                <DetailHeader>Detail Page XXX title</DetailHeader>
-                <Content>
-                    <img src={articleImage} alt="article"/>
-                    <p><b>2017年，衡水中学考上清华北大的人数是176人，</b></p>
-                    <p>2017年，衡水中学考上清华北大的人数是176人，2016年是139人，再往前推到2015年，
-                        这个人数是119人。但是在这些辉煌的名单背后，却是外地来衡水上学人数暴涨，本地人
-                        上好高中越来越艰难的尴尬处境。</p>
-                    <p>2017年，衡水中学考上清华北大的人数是176人，2016年是139人，再往前推到2015年，
-                        这个人数是119人。但是在这些辉煌的名单背后，却是外地来衡水上学人数暴涨，本地人
-                        上好高中越来越艰难的尴尬处境。</p>
-                    <p>2017年，衡水中学考上清华北大的人数是176人，2016年是139人，再往前推到2015年，
-                        这个人数是119人。但是在这些辉煌的名单背后，却是外地来衡水上学人数暴涨，本地人
-                        上好高中越来越艰难的尴尬处境。</p>
-                </Content>
-            </DetailWrapper>
+                <DetailHeader>  {this.props.title}</DetailHeader>
+                <Content dangerouslySetInnerHTML={{__html: this.props.content}}/>
+             </DetailWrapper>
         )
     }
 }
 
-export default Detail;
+const mapState = (state) => ({
+    title: state.getIn(['detail', 'title']),
+    content: state.getIn(['detail', 'content'])
+});
+
+export default connect(mapState, null)(Detail);
