01324c7835fa077618563b94e16f2c4b761af0f7
HuacongXie <solarsys@outlook.com>
2025-03-14 01:42:02 -0400

    1. rename LoadableDetail

--- src/pages/detail/LoadableDetail.js
+++ src/pages/detail/LoadableDetail.js
@@ -1,11 +1,10 @@
 import React, { Suspense } from 'react';
-import Loading from '../../common/loading';
 
 const DetailComponent = React.lazy(() => import('./Detail'));
 
 const LoadableDetail = () => {
     return (
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<div>Loading...</div>}>
             <DetailComponent />
         </Suspense>
     );
