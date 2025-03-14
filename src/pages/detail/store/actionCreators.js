353c686246170db84b362d61180679c873e2e56d
HuacongXie <solarsys@outlook.com>
2025-03-14 00:39:55 -0400

    1.code optimization

--- src/pages/detail/store/actionCreators.js
+++ src/pages/detail/store/actionCreators.js
@@ -6,17 +6,20 @@ const changeDetail = (title, content) => ({
     type: constants.CHANGE_DETAIL,
     title,
     content
-})
+});
 
 
 
 export const getDetail = (id) => {
     return (dispatch) =>{
-        axios.get('/api/detail.json?id='+id).then((res) => {
+        axios.get('/api/detail.json?id='+id)
+            .then((res) => {
             console.log("Detail api data:",res);
             const result = res.data.data;
             dispatch(changeDetail(result.title, result.content));
 
-        })
+        }).catch(() => {
+            console.log("Detail api error");
+        });
     };
 }
