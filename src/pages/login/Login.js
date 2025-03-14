f80f370ec569d2d49d62238d108b1f272281de89
HuacongXie <solarsys@outlook.com>
2025-03-13 23:56:26 -0400

    1.used Navigate component to redirect to home page after login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,27 +2,31 @@ import React, {PureComponent} from 'react';
 import { connect} from "react-redux";
 import { actionCreators } from "./store";
 import {LoginBox, LoginWrapper, Input, Button} from "./style";
-
+import { Navigate } from 'react-router-dom';
 
 class Login extends PureComponent {
     render() {
-        return (
-            <LoginWrapper>
-                <LoginBox>
-                    <Input placeholder="Username" ref={(input)=> {this.account = input}}/>
-                    <Input placeholder="Password" type="password" ref={(input)=> {this.password = input}}/>
-                    <Button onClick={() => this.props.login(this.account, this.password) }>Login</Button>
-                </LoginBox>
-            </LoginWrapper>
-        )
+        const {loginState} = this.props;
+        //if not login, return login page
+        if(!loginState) {
+            return (
+                <LoginWrapper>
+                    <LoginBox>
+                        <Input placeholder="Username" ref={(input)=> {this.account = input}}/>
+                        <Input placeholder="Password" type="password" ref={(input)=> {this.password = input}}/>
+                        <Button onClick={() => this.props.login(this.account, this.password) }>Login</Button>
+                    </LoginBox>
+                </LoginWrapper>
+            )
+        }else {
+            return <Navigate to='/' replace={true} />
+        }
     }
 }
 
-
 const mapState = (state) => ({
- });
-
-
+    loginState: state.getIn(['login', 'login'])
+});
 
 const mapDispatch = (dispatch) => ({
     login(accountElem, passwordElem) {
@@ -30,7 +34,6 @@ const mapDispatch = (dispatch) => ({
         dispatch(actionCreators.login(accountElem, passwordElem));
     }
 });
-
 
 export default connect(mapState, mapDispatch)(Login);
 
